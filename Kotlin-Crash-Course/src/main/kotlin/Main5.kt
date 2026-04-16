1ce7450c153fb7771a5205d535be29c1ce3b0be3
philipfleischer <fleischer1@live.no>
2026-04-16 09:34:47 +0200

    Abstract classses, sealed interfaces completed, starting on enumeration classes -> enum classes

---
+++ Kotlin-Crash-Course/src/main/kotlin/Main5.kt
@@ -0,0 +1,112 @@
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+class Rectangle11(val width: Int, val height: Int)
+
+// THis class should be a data class, since it mainly contains and uses data
+data class Rectangle0(val width: Float, val height: Float) {
+    val diagonal: Float = sqrt(width * width + height * height)
+    val area: Float = width * height
+}
+
+fun functio1() {
+    println("Function1")
+    val rect1 = Rectangle11(width = 5, height = 7)
+    println(rect1.width)
+    println(rect1.height)
+}
+
+
+//New Section:
+fun maxArea1(rect1: Rectangle0, rect2: Rectangle0): Float {
+    val area1 = rect1.area
+    val area2 = rect2.area
+
+    return maxOf(area1, area2)
+}
+
+fun functio2() {
+    println("Function2")
+    val rect2 = Rectangle0(width = 5f, height = 7f)
+
+    println("The diagonal is ${rect2.diagonal}")
+    println("The area is ${rect2.area}" )
+
+    val rect3 = Rectangle0(width = 5f, height = 5f)
+    println("The largest area is ${maxArea1(rect1 = rect2, rect2 = rect3)}")
+}
+
+//New Section:
+data class Rectangle30(val width: Float, val height: Float): Shape0() {
+    val diagonal: Float = sqrt(width * width + height * height)
+    override val area: Float = width * height
+    override val circumference = 2 * width + 2 * height
+}
+
+data class Circle0(val radius: Float): Shape0() {
+    override val area = radius * radius * PI.toFloat()
+    val diameter = 2 * radius
+    override val circumference = 2 * radius
+}
+
+// An abstract class is ... (no instances, inherits)
+abstract class Shape0 {
+    abstract val area: Float
+    abstract val circumference: Float
+
+    // Example of internal state, does not give meaning in this example
+    var counter = 1
+
+    fun inc() {
+        counter++
+    }
+}
+
+fun sumAreas(vararg shapes: Shape0): Double {
+    return shapes.sumOf { currentShape ->
+        currentShape.area.toDouble()
+    }
+}
+
+fun functio3() {
+    println("Function3")
+    val circle = Circle0(5f)
+    println("The circle's radius is ${circle.radius}")
+    println("The circle's area is ${circle.area}")
+    println("The circle's diameter is ${circle.diameter}")
+    val rect1 = Rectangle30(width = 5f, height = 7f)
+    println("sumAreas: ${sumAreas(rect1, circle)}")
+
+    println("rect1.counter = ${rect1.counter}")
+    rect1.inc()
+    println("rect1.counter = ${rect1.counter}")
+
+    // Because of internal state, rect2 has its own counter
+    val rect2 = Rectangle30(width = 5f, height = 5f)
+    println("rect2.counter = ${rect2.counter}")
+    rect2.inc()
+    println("rect2.counter = ${rect2.counter}")
+}
+
+
+
+fun main() {
+//    functio1()
+//    functio2()
+    functio3()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Kotlin-Crash-Course/src/main/kotlin/Main6.kt
@@ -0,0 +1,131 @@
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+// THis class should be a data class, since it mainly contains and uses data
+data class Rectangles(val width: Float, val height: Float) {
+    val diagonal: Float = sqrt(width * width + height * height)
+    val area: Float = width * height
+}
+
+fun fu1() {
+    val rect1 = Rectangle1(width = 5, height = 7)
+    println(rect1.width)
+    println(rect1.height)
+}
+
+
+//New Section:
+fun maxArea(rect1: Rectangles, rect2: Rectangles): Float {
+    val area1 = rect1.area
+    val area2 = rect2.area
+
+    return maxOf(area1, area2)
+}
+
+fun fu2() {
+    val rect2 = Rectangles(width = 5f, height = 7f)
+
+    println("The diagonal is ${rect2.diagonal}")
+    println("The area is ${rect2.area}" )
+
+    val rect3 = Rectangles(width = 5f, height = 5f)
+    println("The largest area is ${maxArea(rect1 = rect2, rect2 = rect3)}")
+}
+
+//New Section:
+data class Rectangles3(val width: Float, val height: Float): Shapes {
+    val diagonal: Float = sqrt(width * width + height * height)
+    override val area: Float = width * height
+    override val circumference = 2 * width + 2 * height
+}
+
+data class Circles(val radius: Float): Shapes {
+    override val area = radius * radius * PI.toFloat()
+    val diameter = 2 * radius
+    override val circumference = 2 * radius * PI.toFloat()
+}
+
+interface Shapes {
+    val area: Float
+    val circumference: Float
+}
+
+sealed interface Shapes2 {
+    val area: Float
+    val circumference: Float
+}
+
+fun printShapes(vararg shapes: Shapes) {
+    for (shape in shapes) {
+        when(shape) {
+            is Circles -> println("This is a Circle SHAPE!")
+            is Rectangles3 -> println("This is a Rectangle SHAPE!")
+        }
+    }
+}
+
+fun printShapes2(vararg shapes: Shapes) {
+    for (shape in shapes) {
+        val output = when(shape) {
+            is Circles -> "This is a Circle SHAPE!"
+            is Rectangles3 -> "This is a Rectangle SHAPE!"
+            else -> "This is not a Circle or Rectangle SHAPE!"
+        }
+        println(output)
+    }
+}
+
+// else -> is not needed since it is a sealed interface and we know which can be used
+fun printShapes3(vararg shapes: Shapes2) {
+    for (shape in shapes) {
+        val output = when(shape) {
+            is Circles -> "This is a Circle SHAPE!"
+            is Rectangles3 -> "This is a Rectangle SHAPE!"
+        }
+        println(output)
+    }
+}
+
+fun sumAreaes(vararg shapes: Shapes): Double {
+    return shapes.sumOf { currentShape ->
+        currentShape.area.toDouble()
+    }
+}
+
+fun fu3() {
+    val circle = Circles(5f)
+    println("The circle's radius is ${circle.radius}")
+    println("The circle's area is ${circle.area}")
+    println("The circle's diameter is ${circle.diameter}")
+    val rect1 = Rectangles3(width = 5f, height = 7f)
+    println("sumAreaes: ${sumAreaes(rect1, circle)}")
+
+    println("PrinShapes1;: ")
+    printShapes(rect1, circle)
+    println("PrinShapes2;: ")
+    printShapes2(rect1, circle)
+    printShapes2(rect1)
+}
+
+
+
+fun main() {
+//    fu1()
+//    fu2()
+    fu3()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
